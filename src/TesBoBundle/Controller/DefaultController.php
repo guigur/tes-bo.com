374911985cec7410df7a6d07d42d0735e099b39f
guigur <guigur13@gmail.com>
2017-02-14 23:16:19 +0100

    revert to a6cadbff7c40fbc75768f0b164a86369f4ffb8e3

--- src/TesBoBundle/Controller/DefaultController.php
+++ src/TesBoBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace TesBoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,6 +21,8 @@ require_once __DIR__ . "vendor/autoload.php";
 use duncan3dc\Speaker\TextToSpeech;
 use duncan3dc\Speaker\Providers\GoogleProvider;
 >>>>>>> c2ccd2d5aca7cb553776f323d1aa8ffae0219ff7
+=======
+>>>>>>> parent of a6cadbf... added form test add media not working
 
 class DefaultController extends Controller
 {
@@ -72,118 +75,4 @@ class DefaultController extends Controller
             return $this->render('TesBoBundle:Default:index.html.twig', array('user' => $user, "gallery" => $gallery));
         }
     }
-
-    public function addMediaTypeAction(Request $request)
-    {
-        // create a task and give it some dummy data for this example
-        $media = new mediaTypes();
-        $form = $this->createFormBuilder($media)
-            ->add('type', TextType::class)
-            ->add('code', TextType::class)
-            ->add('family', TextType::class)
-            ->add('save', SubmitType::class, array('label' => 'add mediatype'))
-            ->getForm();
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($media);
-            $em->flush();
-        }
-
-        return $this->render('TesBoBundle:Default:addMedias.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-
-    public function addMediaAction(Request $request)
-    {
-        // create a task and give it some dummy data for this example
-
-
-        $mediaType = $this->getDoctrine()
-            ->getRepository('TesBoBundle:MediaTypes')
-            ->findOneByType("png");
-
-        $gallerie = $this->getDoctrine()
-            ->getRepository('TesBoBundle:Galleries')
-            ->findOneByName("test");
-
-        $user = $this->getDoctrine()
-            ->getRepository('TesBoBundle:users')
-            ->findOneByPseudo("admin");
-
-        $media = new medias();
-        $form = $this->createFormBuilder($media)
-            ->add('title', TextType::class)
-            ->add('description', TextType::class)
-            ->add('media', FileType::class)
-            ->add('save', SubmitType::class, array('label' => 'add media'))
-            ->getForm();
-
-        $media->setMediaType($mediaType);
-        $media->setDatetimePosted(new \DateTime());
-        $media->setIsVisible(1);
-        $media->setGallerie($gallerie);
-        $media->setUser($user);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($media);
-            $em->flush();
-        }
-
-        return $this->render('TesBoBundle:Default:addMedias.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-/*
-    public function addMediaAction(Request $request)
-    {
-        // create a task and give it some dummy data for this example
-        $media = new Medias();
-        $media->setTitle('Write a blog post');
-
-        //$media->setDescription("desc test");
-        $media->setMediaType(1);
-        $media->setDatetimePosted(new \DateTime());
-        $media->setIsVisible(1);
-        $media->setGallerie(1);
-        $media->setUser(1);
-
-        $form = $this->createFormBuilder($media)
-            ->add('title', TextType::class)
-            ->add('description', DateType::class)
-            ->add('save', SubmitType::class, array('label' => 'add media'))
-            ->getForm();
-
-        return $this->render('TesBoBundle:Default:addMedias.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }*/
-
-
-    public function addMediaRequestAction(Request $request)
-    {
-        $media = new Medias();
-
-        $form = $this->createFormBuilder($media)
-            ->add('title', TextType::class)
-            ->add('description', DateType::class)
-            ->add('save', SubmitType::class, array('label' => 'add media'))
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $media = $form->getData();
-            return $this->redirectToRoute('task_success');
-        }
-
-        return $this->render('default/new.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-
-
 }
